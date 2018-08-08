21e6026172e9299b636ec74845038de93e9d26e1
Vincent Nguyen <vincenttsnguyen@gmail.com>
2018-08-07 19:19:57 -0700

    create files

---
+++ assets/javascript/app.js
@@ -0,0 +1,27 @@
+// var choices = ['First Choice','Second Choice','Third Choice','Fourth Choice']
+// var questions = ['First Question?',2,3,4,5,6,7,8,9,10]
+// var answers = [1,1,1,1,1,1,1,1,1,1]
+// var i = 0
+
+// function checkAnswer(choice){
+//     if (choice == answers[i]){
+//         return true
+//     }
+//     else{
+//         return false
+//     }
+// }
+
+// function start(){
+//     for(var i=0;i<4;i++){
+//         console.log('#choice' + i)
+//         $('#choice' + i).html(choices[i])
+//     }
+//     $('question').html(questions[i])
+// }
+
+// start()
+
+// $('#choice1').on('click',function(){
+//     checkAnswer(this.data)
+// })
